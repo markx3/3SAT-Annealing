de031abf8b6c45d90bd85ec801b943a9b1563f7f
Marcos Felipe Eipper <marcos@eipper.com.br>
2017-12-05 12:49:22 -0200

    algo de errado em acceptance_probability(?)

--- sa.py
+++ sa.py
@@ -1,7 +1,9 @@
-import numpy
+import math
+import numpy as np
 import re
 import time
-from random import randint
+from random import randint, uniform
+from copy import deepcopy
 
 class ThreeSAT:
     def __init__(self, fname):
@@ -21,7 +23,6 @@ class ThreeSAT:
         num_vars = int(splt_hdr[2])
         num_clauses = int(splt_hdr[3])
 
-        print(clauses[0].split()[:-1]) # Splits variables and removes separator
         clauses_res = []
 
         ''' Maps each clause to int list; appends list to list of clauses. '''
@@ -39,8 +40,8 @@ class ThreeSAT:
     def initial_solution(self):
         solution = {}
         for i in range(1, self.num_vars + 1):
-            val = randint(0, 1) == 0
-            solution.update({i: val})
+            val = randint(-1, 1) == 0
+            solution[i] = val
         return solution
 
     ''' Evaluates a given solution; returns a percentage of True clauses '''
@@ -66,34 +67,83 @@ class ThreeSAT:
 
     ''' Perturbates a given solution. ''' # Is it right?
     def perturbation(self, solution):
-        alter_num = randint(1, num_vars//10)
+        new_sol = deepcopy(solution)
+        alter_num = randint(1, 4)
         for _ in range(0, alter_num):
-            altered = randint(1, num_vars+1)
-            val = not solution.get(altered)
-            solution.update({altered: val})
-        return solution
+            altered = randint(1, self.num_vars)
+            val = new_sol.get(altered)
+            new_sol[altered] = not val
+        return new_sol
 
 
 class SimulatedAnnealing:
-    def __init__(self, sat):
+    def __init__(self,
+                 sat,
+                 temp=25000,
+                 maxcalls=500000,
+                 tempmin=1,
+                 alpha=0.0025,
+                 maxpert=10):
         self.sat = sat
         self.clauses = self.sat.clauses
         self.solution = sat.init_solution
+        self.temp = temp
+        self.tempmin = tempmin
+        self.alpha = 1-alpha
+        self.maxcalls = maxcalls
+        self.maxpert = maxpert
+
+    def acceptance_probability(self, old_cost, new_cost, temperature):
+        if new_cost < old_cost:
+            return 1.0
+        else:
+            return math.exp((old_cost - new_cost) / temperature)
+
+    def run(self):
+        solutions = []
+        counter = 0
+        sol_out = self.sat.init_solution
+        sol_out_fo = self.sat.eval(sol_out)
+        sol_aux = {}
+        sol_aux_fo = 0
+        temp = self.temp
+        while temp > self.tempmin and counter < self.maxcalls:
+            i = 1
+            success = 0
+            while i <= self.maxpert:
+                sol = self.sat.perturbation(sol_out)
+                sol_fo = self.sat.eval(sol)
+                delta = sol_out_fo - sol_fo
+                ap = self.acceptance_probability(sol_out_fo, sol_fo, temp)
+                #print(ap)
+                if ap > uniform(0, 1):
+                    sol_out = sol
+                    sol_out_fo = sol_fo
+                i += 1
+            temp = temp*self.alpha
+            counter += 1
+            print(sol_out_fo)
+            if sol_out_fo == 1:
+                break
+        print(sol_out_fo)
+        print(counter)
+        return sol_out
 
 
 class RandomSearch:
-    def __init__(self, sat):
+    def __init__(self, sat, maxcalls=500000):
         self.sat = sat
         self.clauses = self.sat.clauses
+        self.maxcalls = maxcalls
 
     def run(self):
         counter = 0
         sol_out = self.sat.init_solution
         sol_out_fo = self.sat.eval(sol_out)
-        while sol_out_fo != 1: # Precisa mudar p/ num. chamada de função (?)
+        while counter <= self.maxcalls:
             sol = self.sat.initial_solution()
             sol_fo = self.sat.eval(sol)
-            counter += 2
+            counter += 1
             if sol_fo > sol_out_fo:
                 sol_out = sol
                 sol_out_fo = sol_fo
@@ -106,7 +156,8 @@ if __name__ == '__main__':
     sat = ThreeSAT(fname)
     sa = SimulatedAnnealing(sat)
     rs = RandomSearch(sat)
-    print(rs.run())
+    #print(rs.run())
+    print(sa.run())
     # sa.solution = sa.sat.initial_solution()
     # print(sa.clauses)
     # print(sa.solution)
